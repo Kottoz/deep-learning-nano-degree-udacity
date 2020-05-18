ab1b96f48198470010c20121334ad0e5bc9f9139
Kottoz <mohamed.t.elshetry@gmail.com>
2020-05-18 06:17:06 +0200

    update

--- project-dog-classification/face_detector.py
+++ project-dog-classification/face_detector.py
@@ -1,25 +1,41 @@
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # do what you want with frame
-    #  and then save to file
-    cv2.imwrite('Desktop/image.png', frame)
-    if cv2.waitKey(30) & 0xFF == ord('q'): # you can increase delay to 2 seconds here
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-human = face_detector('Desktop/image.png')
-print(if human ? True:False)
+import cv2 
 
 def face_detector(img_path):
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     return len(faces) > 0
+
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+while True:
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'): 
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+            print("Image saved!")
+        
+            break
+        elif key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
+human = face_detector('saved_img.jpg')
+print(human)
